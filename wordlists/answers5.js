07479bbb00655ba511bdb306b715b632645f521c
FPL-Majnun <marius.turcas@yahoo.com>
2025-12-18 17:09:36 +0200

    Create answers5.js

---
+++ wordlists/answers5.js
@@ -0,0 +1,8 @@
+// wordlists/answers5.js
+// Curated DAILY answers (5 letters). Keep this smaller + human-reviewed.
+window.RITUAL_ANSWERS_5 = [
+  "CARTE","MASIV","NOROC","PIESE","SALTA",
+  "MUNTE","LACUL","ROATA","LUMEA","COPAC",
+  "SOARE","VIRUS","LINIE","PUNCT","HARTA",
+  "CINCI","ALTAR"
+];
