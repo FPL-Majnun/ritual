feb1a0651282489a68070359fb8937c307007a9c
FPL-Majnun <marius.turcas@yahoo.com>
2025-12-18 17:10:58 +0200

    Create allowed5.js

---
+++ wordlists/wordlists/allowed5.js
@@ -0,0 +1,8 @@
+// wordlists/allowed5.js
+// Allowed GUESSES (5 letters). This will become BIG (generated).
+window.RITUAL_ALLOWED_5 = [
+  "CARTE","MASIV","NOROC","PIESE","SALTA",
+  "MUNTE","LACUL","ROATA","LUMEA","COPAC",
+  "SOARE","VIRUS","LINIE","PUNCT","HARTA",
+  "CINCI","ALTAR"
+];
